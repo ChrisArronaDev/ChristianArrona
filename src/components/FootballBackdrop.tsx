a2ac5fbbbd71619c47dafa95dec0a309e24530c5
Christian Arrona <arrona.medi.christian10@gmail.com>
2026-09-18 17:28:35 -0600

    refine football anatomy shading and articulated running motion

--- src/components/FootballBackdrop.tsx
+++ src/components/FootballBackdrop.tsx
@@ -13,10 +13,28 @@ export function FootballBackdrop() {
             <stop offset="1" stopColor="#a50044" stopOpacity="0" />
           </radialGradient>
           <linearGradient id="football-rim" x1="0" y1="0" x2="1" y2="1">
-            <stop stopColor="#449dff" />
-            <stop offset=".5" stopColor="#2467bf" />
-            <stop offset="1" stopColor="#f04485" />
+            <stop stopColor="#64809a" stopOpacity=".65" />
+            <stop offset=".5" stopColor="#142131" stopOpacity=".15" />
+            <stop offset="1" stopColor="#946073" stopOpacity=".5" />
           </linearGradient>
+          <linearGradient id="football-fabric" x1="0" x2="1" y1=".2" y2=".6">
+            <stop stopColor="#29415a" />
+            <stop offset=".28" stopColor="#14263b" />
+            <stop offset=".53" stopColor="#351d30" />
+            <stop offset=".76" stopColor="#192338" />
+            <stop offset="1" stopColor="#080f19" />
+          </linearGradient>
+          <linearGradient id="football-skin-light" x1="0" x2="1">
+            <stop stopColor="#645d59" />
+            <stop offset=".27" stopColor="#393c42" />
+            <stop offset=".75" stopColor="#1a202a" />
+            <stop offset="1" stopColor="#30232b" />
+          </linearGradient>
+          <radialGradient id="football-ball-shade" cx=".3" cy=".25">
+            <stop stopColor="#c3cad0" />
+            <stop offset=".65" stopColor="#6d7b86" />
+            <stop offset="1" stopColor="#263142" />
+          </radialGradient>
           <filter
             id="football-light"
             x="-60%"
@@ -24,7 +42,7 @@ export function FootballBackdrop() {
             width="220%"
             height="220%"
           >
-            <feGaussianBlur stdDeviation="3" result="halo" />
+            <feGaussianBlur stdDeviation=".5" result="halo" />
             <feMerge>
               <feMergeNode in="halo" />
               <feMergeNode in="SourceGraphic" />
@@ -54,32 +72,60 @@ export function FootballBackdrop() {
             <g className="football-athlete" filter="url(#football-light)">
               <g className="football-body">
                 <g className="football-arm football-arm-back">
-                  <path d="M7-148Q22-157 29-145L42-107 73-90Q80-80 72-77L33-95Q27-99 25-106Z" />
+                  <path d="M7-148C16-157 24-154 28-143L36-119Q37-114 45-109L66-97Q72-97 76-92L80-86Q81-82 77-82L71-86Q73-80 69-80L61-88 33-101Q28-104 25-114L16-136Z" />
                 </g>
                 <g className="football-leg football-leg-back">
-                  <path d="M1-51 24-49 25-9 5 27-7 21 8-14Z" />
-                  <path
-                    className="football-boot"
-                    d="M-7 19 7 24 13 34 30 38Q37 45 26 47L-3 39-12 31Z"
-                  />
+                  <path d="M1-53Q14-58 24-49C27-39 22-18 17-8Q12 0 4-6L1-27Z" />
+                  <g className="football-calf football-calf-back">
+                    <path d="M4-12Q17-15 17-4C17 8 10 18 8 29L-2 31C-4 19-7 8-3-1Z" />
+                    <path
+                      className="football-sock"
+                      d="M-3 6Q4 9 13 7L8 30-2 32Z"
+                    />
+                    <path
+                      className="football-boot"
+                      d="M-3 28Q2 31 8 28L14 35 27 39Q31 41 29 44L5 44Q-1 43-5 38Z"
+                    />
+                  </g>
                 </g>
-                <path d="M-29-162Q-9-175 13-157L33-120 19-72-23-66-37-113Z" />
-                <path d="M-24-76 20-78 32-47 6-37-4-51-18-35-38-46Z" />
+                <path
+                  className="football-shirt"
+                  d="M-27-160C-19-166-10-167-3-166L12-158Q23-148 24-135L21-119Q17-109 18-97L22-77Q7-69-22-73L-27-93Q-36-109-34-123L-38-141Z"
+                />
+                <path
+                  className="football-fabric-fold"
+                  d="M-25-131Q-14-121-16-104M12-103 4-85M-21-80Q-1-77 16-82"
+                />
+                <path
+                  className="football-shorts"
+                  d="M-24-76Q0-72 20-78L24-64 29-47Q17-40 5-42L-3-55-14-39Q-27-39-36-47L-31-65Z"
+                />
                 <g className="football-leg football-leg-front">
-                  <path d="M-31-51-10-46-22-7-2 27-15 36-42-3Q-46-10-41-23Z" />
-                  <path
-                    className="football-boot"
-                    d="M-16 28-1 24 8 36 26 39Q36 46 23 50L-4 48-17 40Z"
-                  />
+                  <path d="M-31-50Q-19-46-10-47C-10-32-17-16-21-6Q-27 1-35-6C-38-16-36-33-31-50Z" />
+                  <g className="football-calf football-calf-front">
+                    <path d="M-33-12Q-23-16-19-6C-16 4-18 17-14 30L-23 33C-29 23-35 13-36 3Z" />
+                    <path
+                      className="football-sock"
+                      d="M-35 3Q-28 8-18 4L-14 30-23 34Z"
+                    />
+                    <path
+                      className="football-boot"
+                      d="M-24 29Q-19 31-14 28L-5 35 9 39Q14 42 10 45L-11 44Q-21 42-26 37Z"
+                    />
+                  </g>
                 </g>
-                <path d="M-14-162-11-180 6-187 12-167Z" />
-                <path d="M-17-199Q-18-219 0-220 18-222 23-205L21-193 27-186 19-182Q15-169 4-174L-11-180Z" />
+                <path d="M-12-161Q-6-170-9-180L6-184Q4-171 12-164L4-158Z" />
+                <path d="M-12-195C-14-207-5-216 6-213Q21-212 20-199L19-192 24-187Q25-185 20-184L19-178Q14-172 7-175L-4-181Q-12-185-12-195Z" />
                 <path
                   className="football-hair"
-                  d="M-18-199-21-207-15-218-8-223 0-221 7-224 17-217 23-209 20-199 12-203 6-198-3-203-11-193Z"
+                  d="M-13-193C-19-201-14-214-6-216Q0-221 9-217Q21-216 22-205L19-199Q13-204 7-201L1-198-3-192-6-185Q-11-186-13-193Z"
                 />
                 <g className="football-arm football-arm-front">
-                  <path d="M-26-155Q-38-161-43-147L-53-110-78-89Q-83-80-77-76L-67-78-38-99Q-33-103-31-112L-20-140Z" />
+                  <path d="M-26-150C-33-154-40-149-42-141L-49-118Q-50-113-55-108L-70-93-77-90Q-83-85-81-82L-77-83Q-81-77-76-77L-68-81-64-88-40-103Q-36-107-34-115L-25-135Z" />
+                  <path
+                    className="football-shirt"
+                    d="M-29-159Q-42-153-43-140L-40-131-26-128-20-145Z"
+                  />
                 </g>
                 <text
                   className="football-light-number"
